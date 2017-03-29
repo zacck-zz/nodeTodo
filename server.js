783304e4d602f1174b692c826972369d990eff4b
zacck <coderv63@gmail.com>
2017-03-29 06:35:10 +0200

    work on adding routes and route params

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 var express = require('express');
-
 var app  = express();
-
 const PORT = process.env.PORT  || 3000;
 
+var todos = [{
+  description: 'Task we want to get done',
+  completed: false,
+  id: 87686
+},{
+  description: 'Go Surfing',
+  completed: false,
+  id: 87689
+}];
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.send('Todo Api Root');
 });
+
+//GET /todos
+app.get('/todos', (req, res) => {
+  res.send(todos);
+})
+//GET /todos /:id
+app.get('/todos/:id', (req, res) => {
+  res.send(todos[req.params.id]);
+})
 
 
 app.listen(PORT, () => {
