0814fa35ec138a5198a722922f5815fc3352cc02
zacck <coderv63@gmail.com>
2017-04-03 06:30:49 +0200

    work on receiving body data and logging it

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app  = express();
+var bodyParser = require('body-parser');
 const PORT = process.env.PORT  || 3000;
+var todos = [];
+var todoNextId = 1;
 
+
+//set up middleware
+app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.send('Todo Api Root');
 });
@@ -9,7 +15,7 @@ app.get('/', (req, res) => {
 //GET /todos
 app.get('/todos', (req, res) => {
   res.send(todos);
-})
+});
 //GET /todos /:id
 app.get('/todos/:id', (req, res) => {
   var thisTodo = todos.find((todo) => {
@@ -20,8 +26,15 @@ app.get('/todos/:id', (req, res) => {
   } else {
     res.status(404).send('Wow We dont have that todo');
   }
+});
 
-})
+//POST request to /todos
+app.post('/todos', (req, res) => {
+  //use body parser middleware to consume some json
+  var body = req.body;
+  console.log('description: ', body.description);
+  res.json(body);
+});
 
 
 app.listen(PORT, () => {
