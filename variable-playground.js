78547f3ddc7b9c7363f2405001d8f493025ab775
zacck <coderv63@gmail.com>
2017-04-25 15:40:12 +0200

    work on variables passing in javascript

--- variable-playground.js
+++ variable-playground.js
@@ -2,7 +2,7 @@ var person =   {
   name: 'Zacck',
   age: 30
 };
-console.log(person);
+console.log(`Original  - ${person}`);
 //variable assignment
 function updatePerson(obj) {
   // obj= {
@@ -13,4 +13,29 @@ function updatePerson(obj) {
 };
 
 updatePerson(person);
-console.log(person);
+console.log(`Updated -  ${person}`);
+
+
+//Array Example
+
+
+var grades = [15,37];
+
+//takes in the array and the grade to add
+function addGrade(grades) {
+  //push a grade into the array
+  //arr.push(gr);
+  //assign new value
+  grades =  [67,90];
+};
+
+//if we do it like this
+// function addGrade(arr) {
+//   //here we give the array a different variable name
+//   //js will create a new var and assign it to the value of the original
+//   arr =  [78,89];
+// };
+
+addGrade(grades);
+
+console.log(grades);
